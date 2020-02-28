be7d551771c6c4c3a7fd562e6df589ac68694e80
marion vutagwa <marionvutagwa@yahoo.com>
2020-02-28 20:12:51 +0300

    Add new changes to add more context to the page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+/*bussines logic*/
 function order(type, size, crust, toppings){
     this.type = type;
     this.size = size;
@@ -13,24 +14,43 @@ function total (price, quantity, delivery){
     this.delivery = delivery; 
 }
 total.prototype.finalTotal = function() {
-    return this this.price + this.quantity + this.delivery 
+    return this.price + this.quantity + this.delivery 
 }
 var sizePrice = [1300, 900, 680]
 var deliverPrices = [0, 400];
 $(document).ready(function () {
-    $('form#myForm').submit(function (event) {
+    $("form#myForm").submit(function (event) {
         event.preventDefault();
-        var pizzatype = $(#type).val();
-        var pizzatoppings = $(#toppings).val();
-        var pizzacrust = $(#crust).val();
-        var pizzasize = $parseint((#size).val)
-        var pizzaprice = $(#price).val();
-        
-/*business logic*/
-$("#contactform").submit(function(event){
-    event.preventDefault();
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var text = $("#message").val();
-    alert("Hello " + name + " ,Thank You for Contacting Us.");
+        var pizzaType = $("#type").val();
+        var pizzaToppings = $("#toppings").val();
+        var pizzaCrust = $("#crust").val();
+        var pizzaSize = $parseInt(("#size")).val();
+        var pizzaPICK = $parseInt(("#pick")).val();
+        var price = sizePrice[pizzaSize - 1];
+        var DeliveryCost = deliverPrices[pizzaPick - 1];
+    
+    
+    newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaToppings);
+    newTotal = new Total(price, pizzaQuantity, DeliveryCost);
+    if (pizzaPick === 1){
+    alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+    alert("your bill is: " + newTotal.finalTotal() + ".You will receive your pizza in the next few minutes");
+    }else{
+        if(pizzaPick === 2){
+            prompt("Enter where you want your pizza to be delivered");
+            alert("Your order has been received and it will be delivered. Continue to see your order details");
+            alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+            alert("your bill is: " + newTotal.finalTotal() + ".You pizza will be delevered in the next 30minutes");
+        }
+    }
+
+});
+
+$('form#contactform').submit(function(event){
+event.preventDefault();
+var name = $('#name').val();
+var pass = $('#email').val();
+var mess = $('#message').val();
+alert("Hello " + name + " ,Thank You for Contacting Us.");
+});
 });
