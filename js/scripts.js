26b336114459f6346f510373ee0aa09ccb36fc2f
marion vutagwa <marionvutagwa@yahoo.com>
2020-03-03 17:33:55 +0300

    all new changes in the file to add responsive features

--- js/scripts.js
+++ js/scripts.js
@@ -1,59 +1,74 @@
-/*bussines logic*/
-function order(type, size, crust, toppings){
+function Order(type, size, crust, topping) {
     this.type = type;
     this.size = size;
     this.crust = crust;
-    this.toppings = toppings;
+    this.topping = topping;
 }
-order.prototype.fullorder = function() {
-    return this.type + "with this as toppings" + this.toppings + "with" + this.crust + "as the crust" + "and should be " + this.size + "in size"
-}
-function total (price, quantity, delivery){
+
+Order.prototype.fullOrder = function () {
+    return this.type + " with the topping of " + this.topping + " and " + this.crust + " as crust.";
+};
+
+function Total(price, quantity, delivery) {
     this.price = price;
     this.quantity = quantity;
-    this.delivery = delivery; 
+    this.delivery = delivery;
 }
-total.prototype.finalTotal = function() {
-    return this.price + this.quantity + this.delivery 
-}
-var sizePrice = [1300, 900, 680]
-var deliverPrices = [0, 400];
+
+Total.prototype.finalTotal = function () {
+    return this.price * this.quantity + this.delivery;
+};
+
+
+var sizePrice = [1200, 900, 650]
+var deliverPrices = [0, 300];
 $(document).ready(function () {
-    $("form#myForm").submit(function (event) {
+    $('form#myForm').submit(function (event) {
         event.preventDefault();
-        var pizzaType = $("#type").val();
-        var pizzaToppings = $("#toppings").val();
-        var pizzaCrust = $("#crust").val();
-        var pizzaSize = $(parseInt("#size")).val();
-        var pizzaPick = $(parseInt("#pick")).val();
-        var pizzaQuantity = $(parseInt("#qty")).val();
+        var pizzaType = $('#type').val();
+
+        var pizzaSize = parseInt($('#size').val());
+
+        var pizzaCrust = $('#crust').val();
+
+        var pizzaTop = $('#top').val();
+
+        var pizzaQty = parseInt($('#qty').val());
+
+        var pizzaPick = parseInt($('#pick').val());
+
+
         var price = sizePrice[pizzaSize - 1];
+
+
         var DeliveryCost = deliverPrices[pizzaPick - 1];
-    
-    
-    newOrder = new order(pizzaType, pizzaSize, pizzaCrust, pizzaToppings);
-    newTotal = new total(price, pizzaQuantity, DeliveryCost);
-        if (pizzaPick === 1) { 
-        ("Your order is: " + newOrder.fullOrder() + ".continue to see your total bill");
-    alert("Your order is: " + newOrder.fullOrder() + ".continue to see your total bill");
-    alert("your bill is: " + newTotal.finalTotal() + ".You will receive your pizza in the next few minutes");
-    }else{
-        if(pizzaPick === 2) {
-            prompt("Enter where you want your pizza to be delivered");
-            alert("Your order has been received and it will be delivered. Continue to see your order details");
-            alert("Your order is: " + newOrder.fullOrder() + ".continue to see your total bill");
-            alert("your bill is: " + newTotal.finalTotal() + ".You pizza will be delevered in the next 30minutes");
+
+
+
+        newOrder = new Order(pizzaType, pizzaSize, pizzaCrust, pizzaTop);
+        newTotal = new Total(price, pizzaQty, DeliveryCost);
+        if (pizzaPick===1){
+        alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+        alert("your bill is: " + newTotal.finalTotal() + ".You will receive your pizza in the next few minutes");
+        }else{
+            if(pizzaPick===2){
+                prompt("Enter where you want your pizza to be delivered");
+                alert("Your order has been received and it will be delivered. Continue to see your order details");
+                alert("Your oder is: " + newOrder.fullOrder() + ".continue to see your total bill");
+                alert("your bill is: " + newTotal.finalTotal() + ".You pizza will be delevered in the next 30minutes");
+            }
         }
-    }
+
+    });
 
 
-});
 
 $('form#contactform').submit(function(event){
-event.preventDefault();
-var name = $('#name').val();
-var pass = $('#email').val();
-var mess = $('#message').val();
-alert("Hello " + name + " ,Thank You for Contacting Us.");
+    event.preventDefault();
+    var name = $('#name').val();
+    var pass = $('#email').val();
+    var mess = $('#message').val();
+    alert("Hello " + name + " ,Thank You for Contacting Us.");
 });
 });
+
